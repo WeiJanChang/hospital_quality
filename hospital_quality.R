07830e1ca19df4c103bf07bbc7a1d044c2d58ebc
Wei-Jan Chang <cc9868422@gmail.com>
2024-03-15 13:45:58 +0000

    todo: write functions

--- hospital_quality.R
+++ hospital_quality.R
@@ -86,3 +86,15 @@ rankhospital <- function(state, outcome, num = "best") {
 ## Check that state and outcome are valid
 ## Return hospital name in that state with the given rank
 ## 30-day death rate
+
+# Ranking hospitals in all states
+
+# TODO write a function
+
+rankall <- function(outcome, num = "best") {
+  ## Read outcome data
+## Check that state and outcome are valid
+## For each state, find the hospital of the given rank
+## Return a data frame with the hospital names and the
+## (abbreviated) state name
+  }
